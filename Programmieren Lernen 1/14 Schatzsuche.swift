5aa71595f1a6843bb2a2c62e0d6a4ca9a3ff55e9
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-19 16:23:41 +0200

    14 Schatzsuche.swift erstellen

---
+++ Programmieren Lernen 1/14 Schatzsuche.swift
@@ -0,0 +1,29 @@
+func moveThenToggle() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+}
+
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
+
+moveThenToggle()
+turnAround()
+moveForward()
+moveForward()
+moveThenToggle()
+turnAround()
+moveForward()
+moveForward()
+
+turnLeft()
+moveThenToggle()
+moveThenToggle()
+turnAround()
+for i in 1 ... 4 {
+    moveForward()
+}
+moveThenToggle()
+moveThenToggle()
