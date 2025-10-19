24a706ffe5a8c220e34c945b5fc9bdcc71629a97
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-19 16:07:14 +0200

    09 Eine neue Funktion erstellen.swift erstellen

---
+++ Programmieren Lernen 1/09 Eine neue Funktion erstellen.swift
@@ -0,0 +1,18 @@
+func turnRight() {
+    for i in 1 ... 3 {
+        turnLeft()
+    }
+}
+
+moveForward()
+turnLeft()
+moveForward()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+turnLeft()
+moveForward()
+toggleSwitch()
