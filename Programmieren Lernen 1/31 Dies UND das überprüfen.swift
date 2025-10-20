7f9829dd46c7a045c6d6d55ad1691034f31ec71b
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-20 10:07:15 +0200

    31 Dies UND das überprüfen.swift erstellen

---
+++ Programmieren Lernen 1/31 Dies UND das überprüfen.swift
@@ -0,0 +1,17 @@
+for i in 1 ... 7 {
+    moveForward()
+    if isBlockedLeft && isOnGem {
+        turnRight()
+        moveForward()
+        moveForward()
+        toggleSwitch()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnRight()
+    }
+    if isOnGem {
+        collectGem()
+    }
+}
