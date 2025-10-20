d3adb2be2685ede488bc8a12664b7bf23f2ed1da
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-20 17:06:17 +0200

    26 Welten verbinden.swift erstellen

---
+++ Programmieren Lernen 2/26 Welten verbinden.swift
@@ -0,0 +1,14 @@
+let block = Block()
+world.place(block, atColumn: 3, row: 3)
+
+while !isOnClosedSwitch {
+    if isBlocked {
+        if isBlockedLeft {
+            turnRight()
+        } else {
+            turnLeft()
+        }
+    }
+    moveForward()
+}
+toggleSwitch()
