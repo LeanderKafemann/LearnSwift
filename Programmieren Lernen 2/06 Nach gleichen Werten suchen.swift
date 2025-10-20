3763cb348f5b081f37de18b8b115092cea138d66
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-20 11:30:17 +0200

    06 Nach gleichen Werten suchen.swift erstellen

---
+++ Programmieren Lernen 2/06 Nach gleichen Werten suchen.swift
@@ -0,0 +1,13 @@
+let switchCounter = numberOfSwitches
+var gemCounter = 0
+
+while gemCounter < switchCounter {
+    if isOnGem {
+        collectGem()
+        gemCounter += 1
+    }
+    moveForward()
+    if isBlocked {
+        turnRight()
+    }
+}
