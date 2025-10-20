7b9b1b0693d86a486accf63d92211fa6cdee6e0a
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-10-20 11:24:01 +0200

    05 Drei Edelsteine, vier Schalter.swift erstellen

---
+++ Programmieren Lernen 2/05 Drei Edelsteine, vier Schalter.swift
@@ -0,0 +1,21 @@
+var gemCounter = 0
+var switchCounter = 0
+
+while gemCounter < 3 || switchCounter < 4 {
+    moveForward()
+    if isOnGem && gemCounter < 3 {
+        collectGem()
+        gemCounter += 1
+    }
+    if isOnClosedSwitch && switchCounter < 4 {
+        toggleSwitch()
+        switchCounter += 1
+    }
+    if isBlocked {
+        if isBlockedLeft {
+            turnRight()
+        } else {
+            turnLeft()
+        }
+    }
+}
